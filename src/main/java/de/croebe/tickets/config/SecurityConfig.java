a4390c19fedcc4c123f03f3a4be29db67497065b
Wizzel1 <r_christian@gmx.de>
2026-05-20 15:56:49 +0200

    Add Spring Security Configuration

---
+++ src/main/java/de/croebe/tickets/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package de.croebe.tickets.config;
+
+
+import de.croebe.tickets.filters.UserProvisioningFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            UserProvisioningFilter provisioningFilter
+    ) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .addFilterAfter(provisioningFilter, BearerTokenAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+}
