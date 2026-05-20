a57e8e14d31d12fbb3ed04372cfa106b34d064a3
Wizzel1 <r_christian@gmx.de>
2026-05-20 19:15:58 +0200

    Add EventRepository interface

---
+++ src/main/java/de/croebe/tickets/repositories/EventRepository.java
@@ -0,0 +1,12 @@
+package de.croebe.tickets.repositories;
+
+import de.croebe.tickets.domain.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+
+}
