23994cfc0e9a07a46e2ed548cf591055fbd13962
Wizzel1 <r_christian@gmx.de>
2026-05-20 19:33:58 +0200

    Add and implement CreateEventRequest- and ResponseDto classes with

---
+++ src/main/java/de/croebe/tickets/domain/dtos/CreateEventRequestDto.java
@@ -0,0 +1,39 @@
+package de.croebe.tickets.domain.dtos;
+
+
+import de.croebe.tickets.domain.CreateTicketTypeRequest;
+import de.croebe.tickets.domain.entities.EventStatus;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequestDto {
+    @NotBlank(message = "Event name can't be blank")
+    private String name;
+
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    @NotBlank(message = "Venue information is required")
+    private String venue;
+
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+
+    @NotNull(message = "Event status is required")
+    private EventStatus status;
+
+    @NotEmpty(message = "At least one ticket type is required")
+    @Valid
+    private List<CreateTicketTypeRequestDto> ticketTypes;
+}
