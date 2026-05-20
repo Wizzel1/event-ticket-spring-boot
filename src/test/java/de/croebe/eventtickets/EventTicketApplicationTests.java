d2767e6fd4dc84283082f10c4d0adbd776b7dac2
Wizzel1 <r_christian@gmx.de>
2026-05-20 13:00:27 +0200

    Initial commit

---
+++ src/test/java/de/croebe/eventtickets/EventTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package de.croebe.eventtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
