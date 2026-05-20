e8d889214c3214b97f35cc9c7723d0d811b04264
Wizzel1 <r_christian@gmx.de>
2026-05-20 18:31:26 +0200

    Move file

--- src/main/java/de/croebe/tickets/EventTicketApplication.java
+++ src/main/java/de/croebe/tickets/EventTicketApplication.java
@@ -1,4 +1,4 @@
-package de.croebe.eventtickets;
+package de.croebe.tickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventTicketApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(EventTicketApplication.class, args);
     }
 
