65292d2d55701c9585b2c61fe326fac9afcf76fe
Wizzel1 <r_christian@gmx.de>
2026-05-21 14:22:41 +0200

    Implement updateEventForOrganizer method

--- src/main/java/de/croebe/tickets/services/EventService.java
+++ src/main/java/de/croebe/tickets/services/EventService.java
@@ -1,6 +1,7 @@
 package de.croebe.tickets.services;
 
 import de.croebe.tickets.domain.CreateEventRequest;
+import de.croebe.tickets.domain.UpdateEventRequest;
 import de.croebe.tickets.domain.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,4 +15,6 @@ public interface EventService {
     Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
 
     Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
+
+    Event updateEventForOrganizer(UUID organizerId, UUID eventId, UpdateEventRequest request);
 }
