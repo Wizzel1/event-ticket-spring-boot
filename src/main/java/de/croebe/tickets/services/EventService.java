ccfdb5c0208c096e2c50c8a938afabdba432585e
Wizzel1 <r_christian@gmx.de>
2026-05-21 12:26:01 +0200

    Declare and implement getEventsByOrganizer method in EventService

--- src/main/java/de/croebe/tickets/services/EventService.java
+++ src/main/java/de/croebe/tickets/services/EventService.java
@@ -2,9 +2,13 @@ package de.croebe.tickets.services;
 
 import de.croebe.tickets.domain.CreateEventRequest;
 import de.croebe.tickets.domain.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest request);
+
+    Page<Event> getEventsByOrganizer(UUID organizerId, Pageable pageable);
 }
