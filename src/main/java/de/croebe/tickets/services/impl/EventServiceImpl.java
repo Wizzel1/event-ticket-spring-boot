25da9b6b3a6970b063210e7e2e55eb89928496e3
Wizzel1 <43343510+Wizzel1@users.noreply.github.com>
2026-05-21 15:03:35 +0200

    Add delete event endpoint (#4)
    * Declare and implement deleteEventForOrganizer method
    
    * Implement deleteEvent endpoint

--- src/main/java/de/croebe/tickets/services/impl/EventServiceImpl.java
+++ src/main/java/de/croebe/tickets/services/impl/EventServiceImpl.java
@@ -138,4 +138,11 @@ public class EventServiceImpl implements EventService {
 
         return eventRepository.save(existingEvent);
     }
+
+
+    @Override
+    @Transactional
+    public void deleteEventForOrganizer(UUID organizerId, UUID eventId) {
+        getEventForOrganizer(organizerId, eventId).ifPresent(eventRepository::delete);
+    }
 }
