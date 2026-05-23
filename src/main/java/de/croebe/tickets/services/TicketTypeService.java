b807fef7f506c65a0d84de4c1ffe33e2c5df5460
Wizzel1 <r_christian@gmx.de>
2026-05-23 12:51:22 +0200

    Add TicketTypeService and implement purchaseTicket method

---
+++ src/main/java/de/croebe/tickets/services/TicketTypeService.java
@@ -0,0 +1,10 @@
+package de.croebe.tickets.services;
+
+
+import de.croebe.tickets.domain.entities.Ticket;
+
+import java.util.UUID;
+
+public interface TicketTypeService {
+    Ticket purchaseTicket(UUID userId, UUID ticketTypeId);
+}
