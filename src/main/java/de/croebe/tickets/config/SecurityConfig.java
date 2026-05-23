a710de155504d666d1a9c6e9f5d6253ee4f83342
Wizzel1 <43343510+Wizzel1@users.noreply.github.com>
2026-05-23 15:06:55 +0200

    Implement role based security (#10)
    * Add JwtAuthenticationConverter to extract user role from JWT token
    
    * Use JwtAuthenticationConverter in security configuration
    
    * Lock down event endpoint to organizer role
    
    * Remove unused imports

--- src/main/java/de/croebe/tickets/config/SecurityConfig.java
+++ src/main/java/de/croebe/tickets/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import de.croebe.tickets.filters.UserProvisioningFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,15 +17,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(
             HttpSecurity http,
-            UserProvisioningFilter provisioningFilter
+            UserProvisioningFilter provisioningFilter,
+            JwtAuthenticationConverter jwtAuthConverter
     ) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
+                        .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
                         .anyRequest().authenticated())
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
                 .addFilterAfter(provisioningFilter, BearerTokenAuthenticationFilter.class);
 
 
