19725dcfdccf2cd91bfd557931b71ff4193249f4
Wizzel1 <r_christian@gmx.de>
2026-05-24 15:50:26 +0200

    Add method to fetch and decode QR code image for user and ticket

--- src/main/java/de/croebe/tickets/services/QrCodeService.java
+++ src/main/java/de/croebe/tickets/services/QrCodeService.java
@@ -4,6 +4,10 @@ package de.croebe.tickets.services;
 import de.croebe.tickets.domain.entities.QrCode;
 import de.croebe.tickets.domain.entities.Ticket;
 
+import java.util.UUID;
+
 public interface QrCodeService {
     QrCode generateQrCode(Ticket ticket);
+
+    byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }
