c832daa25c9ff499fbda719c6b9dc1e9010e5299
Wizzel1 <r_christian@gmx.de>
2026-05-24 16:16:03 +0200

    Implement getTicketQrCode endpoint in TicketController

--- src/main/java/de/croebe/tickets/controllers/TicketController.java
+++ src/main/java/de/croebe/tickets/controllers/TicketController.java
@@ -3,11 +3,14 @@ package de.croebe.tickets.controllers;
 import de.croebe.tickets.domain.dtos.GetTicketResponseDto;
 import de.croebe.tickets.domain.dtos.ListTicketResponseDto;
 import de.croebe.tickets.mappers.TicketMapper;
+import de.croebe.tickets.services.QrCodeService;
 import de.croebe.tickets.services.TicketService;
 import de.croebe.tickets.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -26,6 +29,7 @@ public class TicketController {
 
     private final TicketService ticketService;
     private final TicketMapper ticketMapper;
+    private final QrCodeService qrCodeService;
 
     @GetMapping
     public Page<ListTicketResponseDto> listTickets(
@@ -50,5 +54,20 @@ public class TicketController {
                 .map(ticketMapper::toGetTicketResponseDto)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(path = "/{ticketId}/qr-codes")
+    public ResponseEntity<byte[]> getTicketQrCode(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID ticketId
+    ) {
+        UUID userId = JwtUtil.parseUserId(jwt);
+        byte[] imageBytes = qrCodeService.getQrCodeImageForUserAndTicket(userId, ticketId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        headers.setContentLength(imageBytes.length);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(imageBytes);
     }
 }
